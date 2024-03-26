8a5d9e4ea3b28e953a8967a2f29135fbfbaf8a4a
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2024-03-26 21:39:16 +0000

    addDD

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,58 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./../styles/App.css";
 
 const App = () => {
-const [users, setUsers]=useState([]);
+  const [users, setUsers] = useState([]);
 
-
-   async function fetchUsers(){
-       try {
-        const res= await axios.get("https://reqres.in/api/users");
-        console.log(res.data.data);
-        setUsers(res.data.data);
-       } catch (error) {
-        console.log(error);
-       }
+  async function fetchUsers() {
+    console.log("axios in");
+    try {
+      const res = await axios.get("https://reqres.in/api/users");
+      console.log(res.data.data);
+      setUsers(res.data.data);
+    } catch (error) {
+      console.log(error);
     }
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h3>Blue Whales</h3>
-        <button className="btn" onClick={fetchUsers}>Get User List</button>
-        <table>
+      {/* Do not remove the main div */}
+      <h3>Blue Whales</h3>
+      <button className="btn" onClick={() => fetchUsers()}>
+        Get User List
+      </button>
+      <table>
+        <thead>
           <tr>
             <th>First Name</th>
             <th>Last Name</th>
-            <th>Eamil</th>
+            <th>Email</th>
             <th>Avatar</th>
           </tr>
-          { users.length!==0 ? users.map(user=>
-          `<tr>
-          <td>${user.first_name}</td>
-          <td>${user.last_name}</td>
-          <td>${user.eamil}</td>
-          <td>${user.avatar}</td>
-           </tr>`
-          ): <p>No data found to display</p>
-              
-          }
-        </table>
-
+        </thead>
+        <tbody>
+          {users.length !== 0 ? (
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.first_name}</td>
+                <td>{user.last_name}</td>
+                <td>{user.email}</td>
+                <td>
+                  <img src={user.avatar} alt={`${user.first_name} Avatar`} />
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4">No data found to display</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
